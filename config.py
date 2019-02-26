19b42241b86ca37fac3d3f4907cd9abb20251256
roxaline <mariadelarosita078@gmail.com>
2019-02-26 17:53:40 +0200

    add a config.py file

---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+
+class Config:
+
+    
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://roxaline:roselyne12@localhost/pitch'
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
+
