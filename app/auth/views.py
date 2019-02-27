09c5117f6bf9f21600607d0e4333aab068c2e5bc
roxaline <45594336+roxaline@users.noreply.github.com>
2019-02-27 12:44:41 +0200

    Update views.py
    add some imports

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,11 @@
 from flask import render_template,redirect,url_for,flash,request
 from . import auth
+from flask_login import login_user
 from flask_login import login_user,logout_user,login_required
 from ..models import User
 from .forms import LoginForm,RegistrationForm
 from .. import db
+from ..email import mail_message
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
